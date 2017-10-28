2d05032c97c057779849db2234cadbb3adaa1b7f
vietanhtran1996 <tranvietanh1996@gmail.com>
2017-10-28 22:45:51 +1300

    Fix unit test for reducing quality by one for normal product

---
+++ csharp/GildedRoseShould.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace csharp
+{
+    [TestFixture]
+    public class GildedRoseShould
+    {
+        [Test]
+        public void ReduceQualityByOneForNormalItem()
+        {
+            IList<Item> items = new List<Item> { new Item { Name = "Normal Item", SellIn = 15, Quality = 20 } };
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.AreEqual(19, items.First().Quality);
+        }
+    }
+}
